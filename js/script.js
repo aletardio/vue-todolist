308649e319d3f73a29aa0b7e0fd505a095f26548
aletardio <ale.tardio99@gmail.com>
2023-12-13 14:51:40 +0100

    Added todo_list

--- js/script.js
+++ js/script.js
@@ -3,7 +3,33 @@ const { createApp } = Vue;
 createApp ({
     data() {
         return {
-            logo:'./img/logoBoolean.png'
+            logo:'./img/logoBoolean.png',
+            todo_list: [
+                {
+                    text: 'Comprare il pane',
+                    done: false
+                },
+                {
+                    text: 'Fare benzina alla macchina',
+                    done: true
+                },
+                {
+                    text: 'Svolgere il progetto di Boolean',
+                    done: false
+                },
+                {
+                    text: 'Andare ad allenamento',
+                    done: false
+                },
+                {
+                    text: 'Cucinare la cena',
+                    done: false
+                },
+                {
+                    text: 'Fare una passeggiata col cane',
+                    done: true
+                },
+            ]
         }
     },
     methods: {
